6be8792bb65dece9f51ca9dcc51b118bb151b64b
mrinaldutt <68439205+mrinaldutt@users.noreply.github.com>
2021-08-29 00:24:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 public static void main(String args[]){
-	System.out.println("------------***** Hello World & Welcoem to Jenkins Training & Made some changes *****--------------");
+	System.out.println("------------***** Hello World & Welcoem to Jenkins Training & Made some changes & WebHook *****--------------");
 }
 }
